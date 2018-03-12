35624f0f3df1d5c624cb0308ee5a965577769b1e
Eric Hartmann <hartmann.eric@gmail.com>
2018-03-12 20:31:42 +0100

    Remove xanitizier from PuginTest
    This plugin is not available for LTS

--- tests/src/test/java/org/sonarqube/tests/plugins/PluginsTest.java
+++ tests/src/test/java/org/sonarqube/tests/plugins/PluginsTest.java
@@ -153,7 +153,8 @@ public class PluginsTest {
     // installPlugin(builder, "vb");
     // FIXME vbnet is disabled as latest release is not using new license manager installPlugin(builder, "vbnet");
     installPlugin(builder, "web");
-    installPlugin(builder, "xanitizer");
+    // Disabled since it's not available for LTS
+    // installPlugin(builder, "xanitizer");
     installPlugin(builder, "xml");
 
     activateLicenses(builder);
